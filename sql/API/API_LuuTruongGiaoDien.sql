efe6b52b8f11fb69b64186ca0237dc53cb60de8d
hoanganh-isme <hoanganhk2.work@gmail.com>
2026-07-13 21:13:10 +0700

    fix: preserve unspecified field metadata values

--- sql/API/API_LuuTruongGiaoDien.sql
+++ sql/API/API_LuuTruongGiaoDien.sql
@@ -1,33 +1,33 @@
-IF OBJECT_ID('API_LuuTruongGiaoDien', 'P') IS NOT NULL
-    DROP PROCEDURE API_LuuTruongGiaoDien;
-GO
-
-CREATE PROCEDURE API_LuuTruongGiaoDien
+CREATE OR ALTER PROCEDURE [dbo].[API_LuuTruongGiaoDien]
     @FormName       varchar(50)      = NULL,
     @FieldName      varchar(50)      = NULL,
     @CaptionVN      nvarchar(255)    = NULL,
     @FormatID       varchar(2)       = NULL,
     @CaptionEN      nvarchar(200)    = NULL,
     @DataSource     nvarchar(500)    = NULL,
-    @IsRequired     bit              = 0,
+    @IsRequired     bit              = NULL,
     @FormPosition   varchar(50)      = NULL,
-    @ShowInAdd      bit              = 1,
-    @ShowInEdit     bit              = 1,
-    @IsReadOnlyEdit bit              = 0,
-    @IsReadOnlyAdd  bit              = 0,
+    @ShowInAdd      bit              = NULL,
+    @ShowInEdit     bit              = NULL,
+    @IsReadOnlyEdit bit              = NULL,
+    @IsReadOnlyAdd  bit              = NULL,
     @ValidateRule   nvarchar(500)    = NULL,
     @DependsOn      varchar(50)      = NULL,
     @VisibleRule    nvarchar(500)    = NULL,
     @OrderNo        int              = NULL,
-    @ShowInFilter   bit              = 0,
+    @ShowInFilter   bit              = NULL,
     @NoResult       bit              = 0
 AS
 BEGIN
     SET NOCOUNT ON;
+    SET XACT_ABORT ON;
 
-    -- Lưu tất cả trực tiếp vào SY_FormatFields (không dùng array trong SY_FrmLstTbl nữa)
-    IF EXISTS (SELECT 1 FROM SY_FormatFields WHERE FieldName = @FieldName AND FormName = @FormName)
-    BEGIN
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        -- Lưu tất cả trực tiếp vào SY_FormatFields (không dùng array trong SY_FrmLstTbl nữa)
+        IF EXISTS (SELECT 1 FROM SY_FormatFields WITH (UPDLOCK, HOLDLOCK) WHERE FieldName = @FieldName AND FormName = @FormName)
+        BEGIN
         UPDATE SY_FormatFields
         SET CaptionVN     = ISNULL(@CaptionVN,     CaptionVN),
             FormatID      = ISNULL(@FormatID,      FormatID),
@@ -45,27 +45,38 @@ BEGIN
             OrderNo       = ISNULL(@OrderNo,       OrderNo),
             ShowInFilter  = ISNULL(@ShowInFilter,  ShowInFilter)
         WHERE FieldName = @FieldName AND FormName = @FormName;
-    END
-    ELSE
-    BEGIN
+        END
+        ELSE
+        BEGIN
         INSERT INTO SY_FormatFields
             (FormName, FieldName, CaptionVN, FormatID, CaptionEN, DataSource,
              IsRequired, FormPosition, ShowInAdd, ShowInEdit, IsReadOnlyEdit, IsReadOnlyAdd,
              ValidateRule, DependsOn, VisibleRule, OrderNo, ShowInFilter)
         VALUES
             (@FormName, @FieldName, @CaptionVN, @FormatID, @CaptionEN, @DataSource,
-             @IsRequired, @FormPosition, @ShowInAdd, @ShowInEdit, @IsReadOnlyEdit, @IsReadOnlyAdd,
-             @ValidateRule, @DependsOn, @VisibleRule, ISNULL(@OrderNo, 0), ISNULL(@ShowInFilter, 0));
-    END
+             COALESCE(@IsRequired, 0), @FormPosition, COALESCE(@ShowInAdd, 1),
+             COALESCE(@ShowInEdit, 1), COALESCE(@IsReadOnlyEdit, 0),
+             COALESCE(@IsReadOnlyAdd, 0), @ValidateRule, @DependsOn, @VisibleRule,
+             COALESCE(@OrderNo, 0), COALESCE(@ShowInFilter, 0));
+        END
 
-    -- Trả về dữ liệu vừa lưu
-    IF @NoResult = 0
-    BEGIN
-        SELECT FormName, FieldName, CaptionVN, FormatID, CaptionEN, DataSource,
-               IsRequired, FormPosition, ShowInAdd, ShowInEdit, IsReadOnlyEdit, IsReadOnlyAdd,
-               ValidateRule, DependsOn, VisibleRule, OrderNo, ShowInFilter
-        FROM SY_FormatFields
-        WHERE FieldName = @FieldName AND FormName = @FormName;
-    END
+        COMMIT TRANSACTION;
+
+        -- Trả về dữ liệu vừa lưu
+        IF @NoResult = 0
+        BEGIN
+            SELECT FormName, FieldName, CaptionVN, FormatID, CaptionEN, DataSource,
+                   IsRequired, FormPosition, ShowInAdd, ShowInEdit, IsReadOnlyEdit, IsReadOnlyAdd,
+                   ValidateRule, DependsOn, VisibleRule, OrderNo, ShowInFilter
+            FROM SY_FormatFields
+            WHERE FieldName = @FieldName AND FormName = @FormName;
+        END
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+
+        THROW;
+    END CATCH
 END
 GO
